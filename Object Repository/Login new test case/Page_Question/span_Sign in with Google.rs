80dcaf69f6dbc4df84bb8dcb5e871c431cda7541
nileshdhale <nilesh.rkr@gmail.com>
2022-03-29 19:36:20 +0530

    Sign in with google

---
+++ Object Repository/Login new test case/Page_Question/span_Sign in with Google.rs
@@ -0,0 +1,96 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>span_Sign in with Google</name>
+   <tag></tag>
+   <elementGuidId>e6ff3d4b-4b73-419e-a193-3fc15a2a4207</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>div.GoogleSignInBtn__Wrapper-sc-inahpz-0.fWlVrp > button[type=&quot;button&quot;] > span</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='__next']/div[2]/div/div/div/div/div[2]/div/div/div/div[2]/button/span</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>span</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Sign in with Google</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;__next&quot;)/div[@class=&quot;wf-section&quot;]/div[@class=&quot;bs&quot;]/div[@class=&quot;AskAnswerInviteHeader__Wrapper-sc-itgzck-0 dmXMLm bs&quot;]/div[@class=&quot;container&quot;]/div[@class=&quot;py-4 row&quot;]/div[@class=&quot;col&quot;]/div[@class=&quot;ask__LoginCardWrapper-sc-lfbvqv-3 jILfXk&quot;]/div[@class=&quot;LoginCard__Card-sc-1j6gauh-0 keAaPl card&quot;]/div[@class=&quot;card-body&quot;]/div[@class=&quot;GoogleSignInBtn__Wrapper-sc-inahpz-0 fWlVrp&quot;]/button[1]/span[1]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='__next']/div[2]/div/div/div/div/div[2]/div/div/div/div[2]/button/span</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='before'])[1]/following::span[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Get 30 days free!'])[1]/following::span[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Step 1 of 3'])[1]/preceding::span[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Enter Critical Questions'])[1]/preceding::span[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Sign in with Google']/parent::*</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]/button/span</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//span[(text() = 'Sign in with Google' or . = 'Sign in with Google')]</value>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Scripts/new login test case/Script1648348081690.groovy
@@ -0,0 +1,58 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://alpha.skillbuilder.io/')
+
+WebUI.maximizeWindow()
+
+WebUI.setText(findTestObject('Object Repository/new login test case/Page_Landing page/input_concat(Your Organization, , s Name)_d_469d2a'), 
+    'tibco')
+
+WebUI.click(findTestObject('Object Repository/new login test case/Page_Landing page/button_Lets GO'))
+
+WebUI.delay(4)
+
+WebUI.click(findTestObject('Object Repository/new login test case/Page_Question/span_Sign in with Google'))
+
+WebUI.delay(3)
+
+WebUI.switchToWindowTitle('Sign in – Google accounts')
+
+WebUI.setText(findTestObject('Object Repository/new login test case/Page_Sign in  Google accounts/input_skillbuilder.io_identifier'), 
+    'nilesh.dhale@kilowott.com')
+
+WebUI.delay(2)
+
+WebUI.verifyElementClickable(findTestObject('new login test case/Page_Sign in  Google accounts/div_Terms of Service_VfPpkd-RLmnJb'))
+
+WebUI.click(findTestObject('Object Repository/new login test case/Page_Sign in  Google accounts/div_Terms of Service_VfPpkd-RLmnJb'))
+
+WebUI.delay(2)
+
+WebUI.setEncryptedText(findTestObject('Object Repository/new login test case/Page_Sign in  Google accounts/input_Too many failed attempts_password'), 
+    'zRFj6Tp9B9qt1LVj8dz10g==')
+
+WebUI.click(findTestObject('Object Repository/new login test case/Page_Sign in  Google accounts/input_Too many failed attempts_c3'))
+
+WebUI.click(findTestObject('Object Repository/new login test case/Page_Sign in  Google accounts/input_Too many failed attempts_c3'))
+
+WebUI.click(findTestObject('Object Repository/new login test case/Page_Sign in  Google accounts/span_Next'))
+
